dd9c025b941c520f6cf5237e7e22aa58f7537002
Piotr Langowski <jesteriv@gmail.com>
2025-08-13 00:17:25 +0000

    1.3.5

--- links.js
+++ links.js
@@ -1,7 +1,7 @@
 const links = {
   6: "https://ecol-unicon.com/dla-branz/infrastruktura-drogowa",
   5: "https://ecol-unicon.com/dla-branz/infrastruktura-kolejowa",
-  1: "https://ecol-unicon.com/dla-branz/infrastruktura-wod-kan",
+  11: "https://ecol-unicon.com/dla-branz/infrastruktura-wod-kan",
   4: "https://ecol-unicon.com/dla-branz/przemysl-i-logistyka",
   7: "https://ecol-unicon.com/dla-branz/energetyka",
   9: "https://ecol-unicon.com/dla-branz/porty-morskie",
